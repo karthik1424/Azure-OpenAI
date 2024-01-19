5868140df13635ba4555ce54f5a81ce7c4583501
karthik1424 <96125038+karthik1424@users.noreply.github.com>
2024-01-19 09:12:36 +0000

    Create dnsprivatezone.tf

---
+++ dnsprivatezone.tf
@@ -0,0 +1,17 @@
+/*
+module "PrivateZone" {
+  source              = "prod-tfe.web.rbsgrp.net/CTO/dns-private-zone/azurerm"
+  version             = "1.0.0"
+  resource_group_name = var.resource_group_name
+  vnet_id             = var.vnet_id
+  location            = var.location
+  tags                = var.standard_tags
+  create_private_zone = false
+
+  #private_dns_zone_names = []
+  #create_dns_a_policy_managedidentity_object_ids = var.create_dns_a_policy_managedidentity_object_ids
+  #user_rbac_client_id = var.user_rbac_client_id
+  #user_rbac_client_secret = var.user_rbac_client_secret
+  #user_tenant_id = var.user_tenant_id
+  #user_subscription_id = var.user_subscription_id
+}*/
